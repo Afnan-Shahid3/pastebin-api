a3b43149a254a35f818df7710921786e2cb9e244
Syed Afnan <afnanhfs016@gmail.com>
2026-07-08 01:30:28 +0500

    Project Setup and Basic Model

--- pastes/models.py
+++ pastes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Paste(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='paste', null = True, blank = True)
+    content = models.CharField(max_length = 100)
+    slug = models.CharField(max_length=10, unique = True, db_index = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+
